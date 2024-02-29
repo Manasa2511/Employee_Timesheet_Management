bdecb65828f3d038ab4fe19154ba2165496d6726
manisha_dev <manishamaheshuni@gmail.com>
2024-02-29 22:08:11 +0530

    basic logout is done

--- JS/Basic-details-js/experiance.js
+++ JS/Basic-details-js/experiance.js
@@ -8,3 +8,7 @@ function showPopup() {
     document.getElementById('warningPopup').style.display = 'none';
     document.querySelector('.overlay').style.display = 'none';
   }
+  // JavaScript funtion for login page
+  function redirectToLogin() {
+    window.location.href = "http://127.0.0.1:5500/Html/login.html";
+  }
